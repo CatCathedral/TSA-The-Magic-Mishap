a919a4e63792852c590659bb700f1e6866c18a57
JonathanJAJ <juajon2008@gmail.com>
2025-02-01 15:09:03 -0600

    Movement Now Working

--- The Magic Mishap TSA/Assets/Scripts/MovementArrows.cs
+++ The Magic Mishap TSA/Assets/Scripts/MovementArrows.cs
@@ -1,48 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementArrows : MonoBehaviour
+public class Player2Movement : MonoBehaviour
 {
-    public float speed;
-    public Rigidbody2D rb;
-    
+    public float moveSpeed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-         float horizontal = Input.GetAxis("Horizontal");
-         float vertical = Input.GetAxis("Vertical");;
-         rb.linearVelocity = new Vector2(horizontal, vertical);
-        
-         if (Input.GetKeyDown(KeyCode.DownArrow))
-         {
-            horizontal = -1;
-         }
-
-         if (Input.GetKeyDown(KeyCode.UpArrow))
-         {
-            horizontal = 1;
-         }
-
-         if (Input.GetKeyDown(KeyCode.LeftArrow))
-         {
-            vertical = -1;
-         }
-
-         if (Input.GetKeyDown(KeyCode.RightArrow))
-         {
-            vertical = 1;
-         }
-
-         if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
-         {
-            vertical = 0;
-         }
-
-         if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.UpArrow))
-         {
-            horizontal = 0;
-         }
+        // Player 2 movement (Arrow keys)
+        float moveX = Input.GetAxisRaw("HorizontalArrow"); // Left and Right arrows
+        float moveY = Input.GetAxisRaw("VerticalArrow");   // Up and Down arrows
+        Vector2 movement = new Vector2(moveX, moveY).normalized;
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 }
--- The Magic Mishap TSA/Assets/Scripts/MovementWASD.cs
+++ The Magic Mishap TSA/Assets/Scripts/MovementWASD.cs
@@ -1,46 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementWASD : MonoBehaviour
+public class Player1Movement : MonoBehaviour
 {
-    public float speed;
-    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-         float horizontal = Input.GetAxis("Horizontal");
-         float vertical = Input.GetAxis("Vertical");;
-         rb.linearVelocity = new Vector3(horizontal, vertical);
-         if (Input.GetKeyDown(KeyCode.S))
-         {
-            vertical = -1;
-         }
-
-         if (Input.GetKeyDown(KeyCode.W))
-         {
-            vertical = 1;
-         }
-
-         if (Input.GetKeyDown(KeyCode.A))
-         {
-            horizontal = -1;
-         }
-
-         if (Input.GetKeyDown(KeyCode.D))
-         {
-            horizontal = 1;
-         }
-
-         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
-         {
-            horizontal = 0;
-         }
-
-         if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
-         {
-            vertical = 0;
-         }
+        // Player 1 movement (WASD)
+        float moveX = Input.GetAxisRaw("Horizontal"); // A and D keys
+        float moveY = Input.GetAxisRaw("Vertical");   // W and S keys
+        Vector2 movement = new Vector2(moveX, moveY).normalized;
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 }
